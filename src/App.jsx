8d16017ab7dce5b40c603abecfa7ad2b4c65a9f3
matias collavini <mncollavini@gmail.com>
2023-12-06 16:58:46 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
-import NavBar from './components/NavBar/Navbar'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 import Error from './components/Error/Error'
 import "./services/firebase"
 import CartContextProvider from './components/context/CartContext'
 import CartList from './components/CartWidget/CartList'
 import Chekin from './components/Chekin/Chekin'
+import NavBar from './components/NavBar/Navbarr'
 
 function App() {
   return (
